2deced10225a0a2aef5ced90fda397e62b70a54c
YuengFu <yuengfu.v@outlook.com>
2021-03-08 14:17:23 +0800

    添加主界面

--- Dairy/MainWindow.xaml.cs
+++ Dairy/MainWindow.xaml.cs
@@ -1,8 +1,7 @@
-﻿using System;
+﻿using MahApps.Metro.Controls;
+using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -10,16 +9,23 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Dairy {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window {
+    public partial class MainWindow : MetroWindow {
         public MainWindow() {
             InitializeComponent();
+        }
+
+        private void LaunchGitHubSite(object sender, RoutedEventArgs e) {
+            // Launch the GitHub site...
+        }
+
+        private void DeployCupCakes(object sender, RoutedEventArgs e) {
+            // deploy some CupCakes...
         }
     }
 }
